5852795d03a429df1c711578aaa4ada714c01455
Anna Kainova <annakainova88@gmail.com>
2023-02-07 11:45:27 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketCost = 13676;
+        int bonusCost = 20;
+        int bonus = ticketCost/bonusCost;
+        System.out.println("The sum of miles: " + bonus);
+    }
+}
